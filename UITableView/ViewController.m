d59656ade0d4afba7b649fd95cb57f1f278ae46f
Icegrave0391 <849680403@qq.com>
2018-04-05 18:55:22 +0800

    增加多组section，索引……

--- UITableView/ViewController.m
+++ UITableView/ViewController.m
@@ -17,27 +17,73 @@
 
 -(UITableView *)tableView{
     if(_tableView == nil) {
-        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain] ;
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped] ;
+        //设置代理和数据源
         _tableView.dataSource = self ;
         _tableView.delegate = self ;
+        //分割线
+        _tableView.separatorInset = UIEdgeInsetsMake(0, 0, 0, 0) ;
+        _tableView.separatorColor = [UIColor brownColor] ;
+        
+        //索引栏
+        _tableView.sectionIndexBackgroundColor = [UIColor clearColor] ;
+        //索引文字
+        _tableView.sectionIndexColor = [UIColor redColor] ;
     }
     return _tableView ;
 }
 
 //dataSource
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 4 ;
+}
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return 20 ;
 }
+//设置section头尾部文字
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    NSString * message = [[NSString alloc] initWithFormat:@"这是第%ld个section头部视图",(long)section] ;
+    return message ;
+}
+-(NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
+    NSString * message = [[NSString alloc] initWithFormat:@"这是第%ld个section尾部视图",(long)section] ;
+    return message ;
+}
 
+//自定义view设置section样式
+/*
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+    UIView * headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 30)] ;
+    headerView.backgroundColor = [UIColor brownColor] ;
+    return headerView ;
+}
+*/
+
+-(UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section{
+    UIView * footerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 30)];
+    footerView.backgroundColor = [UIColor darkGrayColor] ;
+    return footerView ;
+}
+
+//索引样式制定
+-(NSArray<NSString *> *)sectionIndexTitlesForTableView:(UITableView *)tableView{
+    return @[@"s0",@"s1",@"s2",@"s3"] ;
+}
+
+//cell制定
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     //reuse
     static NSString * cellID = @"cell" ;
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID] ;
     if(cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID] ;
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID] ;
     }
     NSString * text = [NSString stringWithFormat:@"%ld",(long)indexPath.row] ;
     cell.textLabel.text = text ;
+    cell.detailTextLabel.text = @"this is a learning test" ;
+    cell.detailTextLabel.textColor = [UIColor grayColor];
+    cell.accessoryType = UITableViewCellAccessoryNone ;
     return cell ;
 }
 
